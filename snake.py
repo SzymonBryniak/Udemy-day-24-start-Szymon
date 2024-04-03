abf72d4bbe1a4f8ca472f422c8e231980dac8c8e
SzymonBryniak <szymonbryniakproject@gmail.com>
2024-04-03 17:48:06 +0100

    continued work on the solution

--- snake.py
+++ snake.py
@@ -4,13 +4,20 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+game_is_on = True
+snake = []
 
-for i in range(3):
+for i in range(5):
     t = Turtle()
+    snake.append(t)
     t.shape('square')
     t.color('green')
     t.penup()
     t.setpos(x=-20*i, y=0)
+
+while game_is_on:
+    for s in snake:
+        s.forward(20)
 
 
 
